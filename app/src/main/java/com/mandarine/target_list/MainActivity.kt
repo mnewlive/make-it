e125885e97c27e7498684e0f2390dc933da0d7cd
Morozov Vadim <morozovvadim91@gmail.com>
2019-03-07 09:37:25 +0200

    Implement auth by firebase email

--- app/src/main/java/com/mandarine/target_list/MainActivity.kt
+++ app/src/main/java/com/mandarine/target_list/MainActivity.kt
@@ -5,20 +5,61 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.firebase.ui.auth.AuthUI
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var mAuthStateListener: FirebaseAuth.AuthStateListener
+    private val RC_SIGN_IN = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        auth = FirebaseAuth.getInstance()
+        mAuthStateListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            val user = firebaseAuth.currentUser
+            if (user != null) {
+                // Sign in logic here.
+            } else {
+                startActivityForResult(
+                    AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setIsSmartLockEnabled(false)
+                        .build(),
+                    RC_SIGN_IN)
+            }
+        }
+
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        auth.addAuthStateListener(mAuthStateListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        auth.removeAuthStateListener(mAuthStateListener)
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {

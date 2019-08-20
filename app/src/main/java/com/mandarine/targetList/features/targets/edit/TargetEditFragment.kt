a8d88f5b173b4add1daba235c3781bc216486d58
Morozov Vadim <morozovvadim91@gmail.com>
2019-08-20 19:23:34 +0300

    Optimized imports

--- app/src/main/java/com/mandarine/targetList/features/targets/edit/TargetEditFragment.kt
+++ app/src/main/java/com/mandarine/targetList/features/targets/edit/TargetEditFragment.kt
@@ -4,22 +4,12 @@ import android.os.Bundle
 import android.support.design.widget.TextInputEditText
 import android.support.v4.app.Fragment
 import android.text.Editable
-import android.text.TextUtils
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import com.mandarine.targetList.R
 import com.mandarine.targetList.constants.KEY_TARGET_GUID
-import com.mandarine.targetList.model.Target
 
 class TargetEditFragment : Fragment(), View.OnClickListener, TargetEditContract {
 

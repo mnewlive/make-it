26aa4bf354ea52021ccbcde2f53f56a82615143c
Morozov Vadim <morozovvadim91@gmail.com>
2019-09-18 21:43:40 +0300

    Display Calendar, also display date when u click on some of date

--- app/src/main/java/com/mandarine/targetList/calendar/CalendarFragment.kt
+++ app/src/main/java/com/mandarine/targetList/calendar/CalendarFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.mandarine.targetList.R
 
 class CalendarFragment : Fragment() {
+
+    private lateinit var calendarView: CalendarView
+    private lateinit var date: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,5 +20,21 @@ class CalendarFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_calendar, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+    }
+
+    private fun setupViews() {
+        calendarView = view!!.findViewById(R.id.calendarView)
+        date = view!!.findViewById(R.id.dateView)
+
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            // Note that months are indexed from 0. So, 0 means January, 1 means february, 2 means march etc.
+            val msg = "Selected date is " + dayOfMonth + "/" + (month + 1) + "/" + year
+            date.setText(msg)
+        }
     }
 }

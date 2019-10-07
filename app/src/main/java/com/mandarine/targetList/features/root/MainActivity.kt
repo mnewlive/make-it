5df61ce9dd6975e386ea52b9d54179451bfcaac9
Morozov Vadim <morozovvadim91@gmail.com>
2019-10-07 16:16:20 +0300

    Added Drawer layout, and layouts for some screen size

--- app/src/main/java/com/mandarine/targetList/features/root/MainActivity.kt
+++ app/src/main/java/com/mandarine/targetList/features/root/MainActivity.kt
@@ -3,11 +3,16 @@ package com.mandarine.targetList.features.root
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.mandarine.targetList.R
 
@@ -18,23 +23,18 @@ class MainActivity : AppCompatActivity(), MainActivityViewContract {
     private lateinit var auth: FirebaseAuth
     private lateinit var mAuthStateListener: FirebaseAuth.AuthStateListener
     private val presenter = MainActivityPresenter(contract = this)
-
-    private lateinit var appBarConfiguration : AppBarConfiguration
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         signIn()
+        setupViews()
+    }
 
-        val host: NavHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
-
-        // Set up Action Bar
-        val navController = host.navController
-
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupBottomNavMenu(navController)
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
     }
 
     override fun onResume() {
@@ -54,6 +54,35 @@ class MainActivity : AppCompatActivity(), MainActivityViewContract {
 
     override fun cancelSignIn() {
         finish()
+    }
+
+    private fun setupViews() {
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+
+        val navController = host.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        val drawerLayout: DrawerLayout? = findViewById(R.id.drawer_layout)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.target_list, R.id.settings_list_fragment, R.id.calendar_fragment),
+            drawerLayout
+        )
+        setupActionBar(navController, appBarConfiguration)
+        setupNavigationMenu(navController)
+        setupBottomNavMenu(navController)
+    }
+
+    private fun setupNavigationMenu(navController: NavController) {
+        val sideNavView = findViewById<NavigationView>(R.id.nav_view)
+        sideNavView?.setupWithNavController(navController)
+    }
+
+    private fun setupActionBar(
+        navController: NavController,
+        appBarConfig: AppBarConfiguration
+    ) {
+        setupActionBarWithNavController(navController, appBarConfig)
     }
 
     private fun setupBottomNavMenu(navController: NavController) {

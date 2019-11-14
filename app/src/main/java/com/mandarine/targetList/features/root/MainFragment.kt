29c5d22503e7e48704ed012df9f4bfbb3451b064
Morozov Vadim <morozovvadim91@gmail.com>
2019-11-14 18:17:11 +0200

    Remove redundant comments and depencies

--- app/src/main/java/com/mandarine/targetList/features/root/MainFragment.kt
+++ app/src/main/java/com/mandarine/targetList/features/root/MainFragment.kt
@@ -19,10 +19,6 @@ class MainFragment : Fragment() {
     private lateinit var auth: FirebaseAuth
     private lateinit var mAuthStateListener: FirebaseAuth.AuthStateListener
 
-    companion object {
-        const val SIGN_IN_RESULT_CODE = 1001
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -62,29 +58,18 @@ class MainFragment : Fragment() {
         if (requestCode == SIGN_IN_RESULT_CODE) {
             val response = IdpResponse.fromResultIntent(data)
             if (resultCode == Activity.RESULT_OK) {
-                // User successfully signed in
-                Log.d("some", "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+                Log.d("some", "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}")
             } else {
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
                 Log.d("some", "Sign in unsuccessful ${response?.error?.errorCode}")
             }
         }
     }
 
     private fun launchSignInFlow() {
-        // Give users the option to sign in / register with their email
-        // If users choose to register with their email,
-        // they will need to create a password as well
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build(),
             AuthUI.IdpConfig.GoogleBuilder().build()
         )
-
-        // Create and launch sign-in intent.
-        // We listen to the response of this activity with the
-        // SIGN_IN_RESULT_CODE code
         startActivityForResult(
             AuthUI.getInstance()
                 .createSignInIntentBuilder()
@@ -93,5 +78,9 @@ class MainFragment : Fragment() {
                 .build(),
             SIGN_IN_RESULT_CODE
         )
+    }
+
+    companion object {
+        const val SIGN_IN_RESULT_CODE = 1001
     }
 }

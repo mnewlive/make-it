346d90a5a3b9d9d3d0750db325f13aedb1bdb141
Vadim Morozov <morozovvadim91@gmail.com>
2020-02-27 14:08:13 +0200

    Added isComplete field in Goal model (#53)
    * Added isComplete field in Goal model

--- app/src/main/java/com/mandarine/targetList/features/targets/edit/TargetEditContract.kt
+++ app/src/main/java/com/mandarine/targetList/features/targets/edit/TargetEditContract.kt
@@ -3,7 +3,13 @@ package com.mandarine.targetList.features.targets.edit
 interface TargetEditContract {
     fun editTarget(targetGuid: String)
     fun deleteTarget()
-    fun updateViewsContent(name: String?, description: String?, deadline: String?, priorityPosition: Int)
+    fun updateViewsContent(
+        name: String?,
+        description: String?,
+        deadline: String?,
+        priorityPosition: Int,
+        isComplete: Boolean
+    )
     fun closeView()
     fun showWarningDialog()
 }
--- app/src/main/java/com/mandarine/targetList/model/Goal.kt
+++ app/src/main/java/com/mandarine/targetList/model/Goal.kt
@@ -1,9 +1,12 @@
 package com.mandarine.targetList.model
 
+import com.google.firebase.database.PropertyName
+
 data class Goal(
     val guid: String = "",
     val name: String = "",
     val description: String = "",
     val deadline: Long = 0L,
-    var priority: Int = 0
+    var priority: Int = 0,
+    @get:PropertyName("isComplete") var isComplete: Boolean = false
 )
